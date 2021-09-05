2e982e8d9b307c6c056db761732fa9301b18414f
Ribert2000 <ribert_rodriguez21@hotmail.com>
2021-09-04 20:23:38 -0500

    segundo commit

--- taller.py
+++ taller.py
@@ -4,4 +4,13 @@ Created on Sat Sep  4 19:56:28 2021
 
 @author: riber
 """
+#Calcule el valor de Y indicando paso a paso como llegó al resultado
 
+#1. y = ( (5+2-5)^2 * 5+8/2 -30 ) / 2 * 5 -3
+y = ( (5+2-5)**2 * 5+8/2 - 30 )  / 2 * 5 - 3 
+print("1. Se resuelve el parentesis: ")
+print("Esto es igual a: ", (5+2-5)**2)
+print("2. Se multiplica la expresión :")
+print("Esto es igual a: ", 45+8/2-30)
+print("3. Se resta: -15-3")
+print("El resultado es: ", y)
